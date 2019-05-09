e3f78ff18fdccfd10cd28d354424518bbe1c059e
Natalya Myunster <natalyamyunster@yahoo.com>
2019-05-09 14:13:58 +0200

    variables

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 document.write("Hello world!!");
 document.write("Hello world!!");
+let number = 21;
+let string = "Hello world! ";
+let floatNumber = 1.23;
+let isTrue;
+isTrue = true;
+
+document.write("Variable number = " + number +"<br />" + string +"="+ floatNumber +"<br />" + isTrue);
