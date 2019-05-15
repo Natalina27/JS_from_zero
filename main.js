f8078bdc4b3bc6a96a2d8dce7f2bd730d56412e4
Natalya Myunster <natalyamyunster@yahoo.com>
2019-05-15 19:04:12 +0200

    Math operations

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
+//3."Hello world!"
 document.write("Hello world!!");
 document.write("Hello world!!");
+
+//4. Variables
 let number = 21;
 let string = "Hello world! ";
 let floatNumber = 1.23;
 let isTrue;
 isTrue = true;
 
-document.write("Variable number = " + number +"<br />" + string +"="+ floatNumber +"<br />" + isTrue);
+document.write("Variable number = " + number +"<br />" + string +"="+ floatNumber +"<br />" + isTrue +"<br />");
+
+//5.Math operation
+let x = 5;
+let y;
+let result;
+
+y = 12;
+result = x/0;
+document.write(result+"<br />");
+document.write("<br />");
+document.write(x + "+" + y + " = " + (x+y) +"<br />");
+let str_1 = "12";
+let str_2 = Number ("24");
+document.write(Number(str_1)+str_2+"<br />");
+let bool;
+let z = 6;
+let d = 7;
+bool = z < d;
+document.write(bool+"<br />");
+
+
+
+
+
