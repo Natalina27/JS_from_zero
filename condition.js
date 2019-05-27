11d9721bd0d64a7025a896c4d963f1d04af48c7f
Natalya Myunster <natalyamyunster@yahoo.com>
2019-05-27 18:24:59 +0200

    switch case break default

--- condition.js
+++ condition.js
@@ -11,3 +11,14 @@ if (x < y) {
 
 }
 
+document.write("<br />");
+
+let result = 9;
+switch (result){
+    case 1:     document.write("result = 1"); break;
+    case 2:     document.write("result = 2"); break;
+    case 3:     document.write("result = 3"); break;
+    case 4:     document.write("result = 4"); break;
+    case 5:     document.write("result = 5"); break;
+    default:  document.write(" result unknown");
+}
