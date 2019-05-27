8d1da8fae4b5aad59d5f169f962796d124238ee9
Natalya Myunster <natalyamyunster@yahoo.com>
2019-05-27 18:18:01 +0200

    else if / else

--- condition.js
+++ condition.js
@@ -1,8 +1,13 @@
-let x = 4;
-let y = 5;
+let x = 7;
+let y = 7;
 if (x < y) {
     document.write("x < y");
     document.write("<br />");
     document.write("4 < 5");
+} else if (x > y) {
+    document.write("x > y")
+} else {
+    document.write("x = y")
+
 }
 
